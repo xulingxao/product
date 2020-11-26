41a7f99d67435d9ecda11dc7515d5a58e38741fa
xulingxao <51937461+xulingxao@users.noreply.github.com>
2020-11-26 13:35:10 +0800

    Create product.js

---
+++ vision-balance-pilates.de/product.js
@@ -0,0 +1,58 @@
+
+$("#product").html("<div class=\"container\">\n" +
+    "            <div class=\"index-title\">\n" +
+    "                <h2> <span>Solutions</span> & Cases</h2>\n" +
+    "                <p>We provide quality and efficient solutions to our global customers </p>\n" +
+    "            </div>\n" +
+    "            <ul class=\"clearfix ul-list\">\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/crushing-equipment/jaw-crushers.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20140617082522623.jpg\" alt=\"Jaw Crushers\" /></a>\n" +
+    "                    <h3><a href=\"/product/crushing-equipment/jaw-crushers.html\" class=\"btn\" rel=\"nofollow\">Jaw Crushers</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/crushing-equipment/tracked-jaw-crusher.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20140705025350433.jpg\" alt=\"Tracked Jaw Crusher\" /></a>\n" +
+    "                    <h3><a href=\"/product/crushing-equipment/tracked-jaw-crusher.html\" class=\"btn\" rel=\"nofollow\">Tracked Jaw Crusher</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/crushing-equipment/sand-maker.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20140617084130257.jpg\" alt=\"Sand Maker\" /></a>\n" +
+    "                    <h3><a href=\"/product/crushing-equipment/sand-maker.html\" class=\"btn\" rel=\"nofollow\">Sand Maker</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/ore-beneficiation/ball-mill.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20140617065726511.jpg\" alt=\"Ball Mill\" /></a>\n" +
+    "                    <h3><a href=\"/product/ore-beneficiation/ball-mill.html\" class=\"btn\" rel=\"nofollow\">Ball Mill</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/dryer-equipment/dryer-machine.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20130916045647250.jpg\" alt=\"Dryer Machine\" /></a>\n" +
+    "                    <h3><a href=\"/product/dryer-equipment/dryer-machine.html\" class=\"btn\" rel=\"nofollow\">Dryer Machine</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/product/ore-beneficiation/rotary-kiln.html\" class=\"img\" rel=\"nofollow\"><img src=\"/images/20140820065745790.jpg\" alt=\"Rotary Kiln\" /></a>\n" +
+    "                    <h3><a href=\"/product/ore-beneficiation/rotary-kiln.html\" class=\"btn\" rel=\"nofollow\">Rotary Kiln</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/stone-production-line.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180512140951.jpg\" alt=\"Stone production line\" /></a>\n" +
+    "                    <h3><a href=\"/solution/stone-production-line.html\" class=\"btn\" rel=\"nofollow\">Stone production line</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/grinding-production-line.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180512141042.jpg\" alt=\"Grinding production line\" /></a>\n" +
+    "                    <h3><a href=\"/solution/grinding-production-line.html\" class=\"btn\" rel=\"nofollow\">Grinding production line</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/sand-making-plant.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180512141004.jpg\" alt=\"Sand making plant\" /></a>\n" +
+    "                    <h3><a href=\"/solution/sand-making-plant.html\" class=\"btn\" rel=\"nofollow\">Sand making plant</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/cement-plant-production-line.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180512141107.jpg\" alt=\"Cement plant production line\" /></a>\n" +
+    "                    <h3><a href=\"/solution/cement-plant-production-line.html\" class=\"btn\" rel=\"nofollow\">Cement plant production line</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/ore-beneficiation-plant.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180629113818.jpg\" alt=\"Ore beneficiation plant\" /></a>\n" +
+    "                    <h3><a href=\"/solution/ore-beneficiation-plant.html\" class=\"btn\" rel=\"nofollow\">Ore beneficiation plant</a></h3>\n" +
+    "                </li>\n" +
+    "                                <li>\n" +
+    "                    <a href=\"/solution/flotation-separation-production-line.html\" class=\"img\" rel=\"nofollow\"><img src=\"/uploadfile/cx_small_20180512141022.jpg\" alt=\"Flotation separation production line\" /></a>\n" +
+    "                    <h3><a href=\"/solution/flotation-separation-production-line.html\" class=\"btn\" rel=\"nofollow\">Flotation separation production line</a></h3>\n" +
+    "                </li>\n" +
+    "                            </ul>\n" +
+    "        </div>")
+
